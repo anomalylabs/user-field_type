f61ddef547b15e990e8c84a97343c2aeba1a787d
MarceauKa <marceau@casals.fr>
2015-07-15 18:15:28 +0200

    Ajout de la traduction française

---
+++ resources/lang/fr/addon.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'        => 'Utilisateur',
+    'description' => 'Type de champs pour la sélection d\'un utilisateur.',
+];
---
+++ resources/lang/fr/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'role'       => [
+        'label'        => 'Avec le rôle',
+        'instructions' => 'Vous pouvez choisir un rôle pour retourner uniquement les utilsateurs ayant ce rôle.',
+        'placeholder'  => 'Choisissez un rôle...'
+    ],
+    'permission' => [
+        'label'        => 'Avec la permission',
+        'instructions' => 'Vous pouvez choisir une permission pour retourner uniquement les utilsateurs ayant cette permission.'
+    ]
+];
